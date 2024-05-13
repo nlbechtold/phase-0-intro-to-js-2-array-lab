435c2f3d87f84b53cb5b1dfcc0b58fe5ab79f62a
Nancy Bechtold <nancyleebechtold@gmail.com>
2024-05-13 13:34:07 -0700

    arrays lab

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob");
+
+}
+function destructivelyRemoveLastCat(name) {
+    cats.pop("Garfield");
+}
+function destructivelyRemoveFirstCat(name) {
+    cats.shift("Milo");
+}
+
+function appendCat(name) {
+const catsModified = [...cats, "Broom"];
+return catsModified
+}
+
+function prependCat(name) {
+    const catsModifiedDos = ["Arnold", ...cats,];
+    return catsModifiedDos
+    }
+function removeLastCat(name) {
+    const catsModifiedTres = cats.slice (0, -1);
+    return catsModifiedTres
+
+}   
+function removeFirstCat(name) {
+    const catsModifiedQuatro = cats.slice (1);
+    return catsModifiedQuatro
+
+}   
+
